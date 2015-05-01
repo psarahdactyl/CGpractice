083b6d68e7facc46a24cbc7b52a857d92b8b076c
Sarah Kushner <sak338@drexel.edu>
2015-04-30 22:44:32 -0400

    homework 4 done.

---
+++ hw4/include/Circle.h
@@ -0,0 +1,19 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include "Shape.h"
+
+class Circle: public Shape
+{
+	public:
+		void genPoints(GLfloat x, GLfloat y, GLfloat r); 
+		Circle(GLfloat x, GLfloat y, GLfloat r, GLenum mode, vec4 color);
+		Circle(GLfloat x, GLfloat y, float scaleX, float scaleY, GLfloat r, GLenum mode, vec4 color);
+		~Circle();
+	private:
+		float scaleX, scaleY;
+};
+
+
+#endif
+
